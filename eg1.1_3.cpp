e164371ef5ec939430a89b1dc004943bf1146d7d
Ying Genqiu <ycicada@163.com>
2017-05-23 21:17:09 +0800

    change some cpp

--- eg1.1_3.cpp
+++ eg1.1_3.cpp
@@ -1,24 +1,20 @@
-//eg1.1_2.cpp
+//eg1.1_3.cpp
  //给出平面上两点的坐标，求出两点之间的曼哈顿距离。
  //d=|x1-x2|+|y1-y2|
  #include <iostream>
  using namespace std;
  
+ double abs(double x)
+{
+    return x>0?x:-x;
+ } 
+ 
  int main()
  {
  	double x1,y1,x2,y2,dx,dy;
  	cin>>x1>>y1>>x2>>y2;
- 	
-	//if(x1>x2) 
-	//    dx=x1-x2;
- 	//else
-	//    dx=x2-x1;
-	dx=(x1>x2)?x1-x2:x2-x1;
- 	//if(y1>x2)
- 	//    dy=y1-y2;
- 	//else
- 	//    dy=y2-y1;
- 	dy=(y1>y2)?y1-y2:y2-y1;
+ 	dx=abs(x1-x2);
+ 	dy=abs(y1-y2);
  	cout<<dx+dy<<endl;
  	return 0;
  	
