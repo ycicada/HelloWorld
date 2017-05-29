808e6130163b08771bdce090c9557b81468a9674
Ying Genqiu <ycicada@163.com>
2017-05-29 21:25:30 +0800

    for循环的第一个例子

--- eg_for01.cpp
+++ eg_for01.cpp
@@ -1,10 +1,11 @@
+//for循环的第一个例子 
 #include<iostream> 
 using namespace std;
 
 int main()
 {
-	for(int i=0;i<10;i++)
+	for (int i=0; i<10; i++)//i++相当于i=i+1
 	  cout<<i<<endl;
+	  
 	return 0;
-	
 }
