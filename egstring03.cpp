0274e8b31ed206c5a5e41aabee0f8520682e4a28
Ying Genqiu <ycicada@163.com>
2017-05-30 20:12:33 +0800

    用两种方法实现字符串的统计

--- egstring03.cpp
+++ egstring03.cpp
@@ -1,16 +1,16 @@
 //输入一个字串，找出第一个只有单次出现的字母并输出，若不符合条件，输出NO
-//这里用了双重循环，单循环如何实现？ 
+//第一种方法用了双重循环，第二种方法用了单循环实现。 
+ 
  
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int main(int argc,char *argv[]) 
+string once1(string s)
 {
-	string s;
-	int c=0;
-	cin>>s;
+	int c;
+	string s2="";
 	for (int i=0;i<s.size();i++)
 	{
 		c=0;
@@ -20,10 +20,38 @@ int main(int argc,char *argv[])
 	   }
 	   if(c==1) 
 	   {
-	   cout<<s[i];	   
-	   return 0;
+	   //cout<<s[i];
+	   s2+=s[i];
+	   return s2;
 	   }
     }    
-    cout<<"NO";
+    return "NO";	
+}
+
+string once2(string s)
+{
+	int c,a[26]={0};
+	string s2="";
+	for (int i=0;i<s.size();i++)
+	{
+		a[s[i]-'a']++;
+    }    
+    for (int j=0;j<26;j++)
+      if(a[j]==1) 
+      {
+	   s2+=char(j+'a');
+	   return s2;
+	  }
+        
+	 return "NO";	
+}
+
+
+int main(int argc,char *argv[]) 
+{
+    string s;
+	cin>>s;
+	cout<<once1(s)<<endl;//第一种方法 
+    cout<<once2(s)<<endl;//第二种方法 
 	return 0;
 }
