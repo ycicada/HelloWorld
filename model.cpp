6c9e2c1f23dd41a3a5e240bc2526f06305d7067f
ycicada <ycicada@163.com>
2017-08-17 10:51:15 +0800

    模板

---
+++ model.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <iomanip>
+//#include <cstdio>
+//#include <string>
+//#include <cmath>
+//#include <fstream>
+
+using namespace std;
+
+//ifstream fin("a.in");
+//ifstream fout("a.out");
+
+/*程序说明：*/
+int main()
+{
+    ios_base::sync_with_stdio(false);//解绑scanf&printf
+    cin.tie(0);//解除cin&cout绑定
+    //控制输出格式 
+	//cout<<fixed;//浮点数常规输出 
+    //cout<<scientific;//科学计算法输出 
+	//cout<<precision(5);//小数点保留位数
+	 
+	cout<<"Hello world!"<<endl; 
+    
+	return 0;
+}
+
